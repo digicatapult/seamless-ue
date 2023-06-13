0901abfa5984e22a7b2f00b259851503f0952661
grigtod <grigtod@gmail.com>
2023-06-13 14:27:06 +0100

    Add file conversion and file loading (#14)
    * Add file conversion and file loading
    
    * Delete HoloLensEngine.ini

---
+++ Source/SEAMless/Private/ExternalProcessRun.cpp
@@ -0,0 +1,19 @@
+// Copyright 2023 Digital Catapult
+
+
+#include "ExternalProcessRun.h"
+
+void UExternalProcessRun::RunProcess(FString batPath, TArray<FString> args, int32& id, UProcessPipe* pipe) // priority: 2 fastest, -2 slowest
+{
+	if (pipe) pipe->StartProcReader(); 
+
+	FString combinedArgs = "";
+	for (int32 i = 0; i < args.Num(); i++)
+	{
+		if (i != 0) combinedArgs = combinedArgs + " ";
+		combinedArgs = combinedArgs + args[i];
+	}
+	uint32 uintid = 0;
+	FPlatformProcess::CreateProc(*batPath, *combinedArgs, false, false, false, &uintid, 2, nullptr, (pipe && pipe->IsValid()) ? pipe->WriteProcessPipe : nullptr);
+	id = uintid;
+}
