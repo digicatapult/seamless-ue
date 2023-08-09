84eea024e660d121bd767d9903df4c983ee47768
grigtod <grigtod@gmail.com>
2023-08-09 11:45:55 +0100

    Add e57 support

--- Other/ConvertPLYToLAS.bat
+++ Other/ConvertPLYToLAS.bat
@@ -1,2 +1,2 @@
 @echo off
-"C:\Program Files\CloudCompare\CloudCompare.exe" -SILENT -C_EXPORT_FMT LAS -O %1 -SAVE_CLOUDS FILE %2
+"C:\Program Files\CloudCompare\CloudCompare.exe" -SILENT -C_EXPORT_FMT E57 -O %1 -SAVE_CLOUDS FILE %2
